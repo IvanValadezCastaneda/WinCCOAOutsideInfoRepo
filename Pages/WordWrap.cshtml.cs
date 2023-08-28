392c7bbc428722b99032cba9847611b8fb4013a9
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2023-08-28 08:50:26 -0600

    Word Wrap Trick introduction

---
+++ Pages/WordWrap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WinCCOAOutsideInfoRepo.Pages
+{
+    public class WordWrapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
