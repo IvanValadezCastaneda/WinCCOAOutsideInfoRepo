cddbd7a39f7ccb1426fbfb1e62ba505e7946e623
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2023-09-18 09:00:21 -0600

    $-Parameters in Runtime

---
+++ Pages/RuntimeTips.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WinCCOAOutsideInfoRepo.Pages
+{
+    public class RuntimeTipsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
