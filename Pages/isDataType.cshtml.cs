4e5e51c8337fbc8393fc026335c8114c023a372f
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2023-12-18 09:19:53 -0600

    Data Type Got and SQL started

--- Pages/isDataType.cshtml.cs
+++ Pages/isDataType.cshtml.cs
@@ -5,8 +5,28 @@ namespace WinCCOAOutsideInfoRepo.Pages
 {
     public class isDataTypeModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string UserInput { get; set; }
+        public string DataTypeMessage { get; set; }
+
+        public void OnPost()
         {
+            if (int.TryParse(UserInput, out int _))
+            {
+                DataTypeMessage = "int";
+            }
+            else if (double.TryParse(UserInput, out double _))
+            {
+                DataTypeMessage = "double";
+            }
+            else if (bool.TryParse(UserInput, out bool _))
+            {
+                DataTypeMessage = "bool";
+            }
+            else
+            {
+                DataTypeMessage = "string";
+            }
         }
     }
 }
