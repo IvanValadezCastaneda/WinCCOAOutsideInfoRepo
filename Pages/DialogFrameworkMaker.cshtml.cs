0706fbba610da847a0b24e9eaeda168cbcadbbb7
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2024-01-04 09:19:16 -0600

    DialogFrameworkMaker

---
+++ Pages/DialogFrameworkMaker.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WinCCOAOutsideInfoRepo.Pages
+{
+    public class DialogFrameworkMakerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
