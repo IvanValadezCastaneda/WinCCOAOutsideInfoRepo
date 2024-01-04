17ddab9ca995b1faa01528c34c55ef3f454aa1a0
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2024-01-04 09:43:03 -0600

    First Ammend, Almost Finished

--- Pages/DialogFrameworkMaker.cshtml.cs
+++ Pages/DialogFrameworkMaker.cshtml.cs
@@ -1,12 +1,59 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
 
 namespace WinCCOAOutsideInfoRepo.Pages
 {
     public class DialogFrameworkMakerModel : PageModel
     {
-        public void OnGet()
+        [BindProperty] public string DialogType { get; set; }
+        [BindProperty] public string Text { get; set; }
+        [BindProperty] public string Title { get; set; }
+        [BindProperty] public string ButtonTextOk { get; set; }
+        [BindProperty] public string ButtonTextNOk { get; set; }
+        [BindProperty] public string TitleColor { get; set; }
+        // Add other properties as necessary
+
+        public string GeneratedScript { get; private set; }
+
+        public void OnPost()
         {
+            var mappingEntries = new Dictionary<string, string>
+            {
+                {"text", Text},
+                {"title", Title},
+                {"buttonTextOk", ButtonTextOk},
+                {"buttonTextNOk", ButtonTextNOk},
+                {"titleColor", TitleColor},
+                // Add other dialog options as key-value pairs
+            };
+
+            GeneratedScript = GenerateDialogScript(DialogType, mappingEntries);
+        }
+
+        private string GenerateDialogScript(string dialogType, Dictionary<string, string> options)
+        {
+            var scriptBuilder = new StringBuilder();
+            scriptBuilder.AppendLine("#uses \"classes/DialogFramework\"");
+            scriptBuilder.AppendLine("main()");
+            scriptBuilder.AppendLine("{");
+            scriptBuilder.Append($"  int answer = DialogFramework::{dialogType}(");
+            scriptBuilder.Append("makeMapping(");
+
+            foreach (var entry in options)
+            {
+                if (!string.IsNullOrEmpty(entry.Value))
+                {
+                    scriptBuilder.Append($"\"{entry.Key}\", \"{entry.Value}\", ");
+                }
+            }
+
+            scriptBuilder.Remove(scriptBuilder.Length - 2, 2); // Remove trailing comma and space
+            scriptBuilder.AppendLine("));");
+            scriptBuilder.AppendLine("  DebugN(\"Button pressed: \" + answer);");
+            scriptBuilder.AppendLine("}");
+
+            return scriptBuilder.ToString();
         }
     }
 }
