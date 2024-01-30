6058afff82d1de4626451abdb0df248f1d9840ca
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2024-01-30 09:00:17 -0600

    Misc Page Added not routed

---
+++ Pages/MiscFacts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WinCCOAOutsideInfoRepo.Pages
+{
+    public class MiscFactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
